bff9e4453c38127b1785e8c6d2c8d8d38fe1243d
ritika7 <ritikabhawsar7@gmail.com>
2025-08-08 23:55:31 +0400

    Added firebase files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  swcMinify: true,
+  output: "export",
+  images: {
+    unoptimized: true,
+    formats: ["image/avif", "image/webp"],
+  },
 };
 
 export default nextConfig;
