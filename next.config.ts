fce15a2b36d16d2f14cc0136ed0c703af324fd6c
ritika7 <ritikabhawsar7@gmail.com>
2025-08-09 00:42:23 +0400

    Update eslint for no-explicit-any

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  swcMinify: true,
-  output: "export",
-  images: {
-    unoptimized: true,
-    formats: ["image/avif", "image/webp"],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
