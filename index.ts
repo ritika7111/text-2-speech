a98535fa5483e73d5475794a91c4a4cb5df86b63
ritika7 <ritikabhawsar7@gmail.com>
2025-08-10 16:47:10 +0400

    added index file

---
+++ index.ts
@@ -0,0 +1,10 @@
+const functions = require('firebase-functions');
+const { default: next } = require('next');
+
+const isDev = process.env.NODE_ENV !== 'production';
+const app = next({ dev: isDev, conf: { distDir: '.next' } });
+const handle = app.getRequestHandler();
+
+exports.nextServer = functions.https.onRequest((req : any, res : any) => {
+  return app.prepare().then(() => handle(req, res));
+});
